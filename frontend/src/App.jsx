ac3ec8defb30ecbf170fd45302406a439b9ad943
Md Musfiqur Rahman <musfiqurm661@gmail.com>
2025-12-18 01:43:38 +0600

    Update API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1707,7 +1707,7 @@ const DefectoMCUDashboardLight = () => {
 
         try {
             // HOSTING NOTE: Upload korar por nicher Link ta change korte hobe
-            const response = await fetch('http://127.0.0.1:8000/predict', {
+            const response = await fetch('https://defectomcu-api.onrender.com/predict', {
                 method: 'POST',
                 body: formData,
             });
